ce1929be383530a0aeb55c9c85ac70b04b1970f4
SirOlash <Olasupoemmanuel30@gmail.com>
2026-01-29 18:16:51 +0100

    fixed query bug

--- src/main/java/com/onepipe/controllers/PaymentController.java
+++ src/main/java/com/onepipe/controllers/PaymentController.java
@@ -59,8 +59,8 @@ public class PaymentController {
     }
 
     @PostMapping("/{id}/query")
-    public ResponseEntity<Payment> queryPaymentStatus(@PathVariable String onePipeId) {
-        Payment updatedPayment = paymentService.queryAndFixPaymentStatus(onePipeId);
+    public ResponseEntity<Payment> queryPaymentStatus(@PathVariable String id) {
+        Payment updatedPayment = paymentService.queryAndFixPaymentStatus(id);
         return ResponseEntity.ok(updatedPayment);
     }
 }
